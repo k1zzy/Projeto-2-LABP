919e547b3ae77b69242eef43bdf285cee5be36fe
rodrigo <rodrigo.bb@outlook.pt>
2024-03-23 11:58:22 +0000

    criaçao das classes a entregar

---
+++ project2LabP/src/project/ComplexoConcreto.java
@@ -0,0 +1,107 @@
+package project;
+
+public class ComplexoConcreto implements Complexo {
+
+	@Override
+	public double re() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double im() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double rho() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double theta() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double norma() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Complexo soma(Complexo outro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Complexo produto(Complexo outro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Complexo potencia(double x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Complexo quociente(Complexo outro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Complexo conjugado() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean ehReal() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ehReal(double erro) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ehZero() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ehZero(double erro) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ehIgual(Complexo outro) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ehIgual(Complexo outro, double erro) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String repTrigonometrica() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ project2LabP/src/project/PolinomioVetor.java
@@ -0,0 +1,99 @@
+package project;
+
+public class PolinomioVetor implements Polinomio {
+
+	public PolinomioVetor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int grau() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Complexo coef(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean ehZero() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ehZero(double erro) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ehConstante() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Polinomio escalar(Complexo f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Polinomio simetrico() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Polinomio soma(Polinomio p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Polinomio subtraccao(Polinomio p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Polinomio produto(Polinomio p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Complexo avalia(Complexo x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Polinomio derivada() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Polinomio copia() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean ehIgual(Polinomio p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ehIgual(Polinomio p, double erro) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
